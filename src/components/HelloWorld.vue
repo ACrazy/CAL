998c2f6ff68473f0ef77351680f7f9cc88fcb187
acrazy <2265373762@qq.com>
2019-11-23 13:50:40 +0800

    init project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    fuck
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
