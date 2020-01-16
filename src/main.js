9641ef5fbacda0104e7b92c2157a43fa17c5b5b1
acrazy <2265373762@qq.com>
2020-01-16 23:13:55 +0800

    解决因为main.js引入顺序问题导致打包样式错乱，解决elementUI图标打包后显示异常

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
-import App from './App'
-import router from './router'
-import store from './store/index.js'
-import Vuex from 'vuex'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import App from './App'
+import Vuex from 'vuex'
+import store from './store/index.js'
+import router from './router'
 
 Vue.use(ElementUI)
 Vue.use(Vuex)
